17209ba732dfbed13a9eee9e8dff6d5852c33337
Robson Coura <robson_coura@yahoo.com.br>
2023-06-15 22:52:43 -0300

    Criacao da class Usuario e implementando atributos id, nome, email e senha.

---
+++ forum/src/main/java/br/com/alura/forum/modelo/Usuario.java
@@ -0,0 +1,73 @@
+package br.com.alura.forum.modelo;
+
+public class Usuario {
+
+    //Atributos
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    /*Método hashCode serve como uma "forma" do Java organizar e agrupar os objetos da lista,
+   com a intenção de localizar mais rápido um determinado objeto.
+   */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    //Equals é um padrão universal para comparação de objetos
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    // Método Getters(recuperem os valores contidos nos atributos) e Setters(modificar valores contidos nos atributos)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
